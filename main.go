24d14f94c9ae54c302915f544c8ad8763882ad6c
sjpark9365 <53499114+sjpark9365@users.noreply.github.com>
2019-07-31 10:52:57 +0900

    Change the message Text

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World, eks-workshop"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
